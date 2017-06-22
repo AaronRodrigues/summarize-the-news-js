f8a03b514aaaa391cfbbd582a5c101228e78c455
“rogrenke” <roland@rolandgrenke.com>
2017-06-22 11:26:14 +0100

    Adds simple static div element which will be used to display the news - element will be replaced by dynamically created elements from API

--- test/featureTest.js
+++ test/featureTest.js
@@ -6,4 +6,20 @@ function checkHeadOnPage() {
   page.hasContent(header, "See the latest news!", arguments.callee.name);
 };
 
+function checkNewsOnPage() {
+  var news = document.getElementsByClassName("news");
+  if (news.length === 0) {
+    throw new Error ("There are no news!");
+  } else {
+    console.log(arguments.callee.name + " test passed!")
+  }
+};
+
+// function checkNewsHasHeadline() {
+//   var headline = document.getElement
+// }
+// #1 check if there is elements of headlines list
+// #2
+
 checkHeadOnPage();
+checkNewsOnPage();
