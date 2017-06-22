e50e871404e6c40ee4750b36f07ca4cdeaf741f0
“rogrenke” <roland@rolandgrenke.com>
2017-06-22 12:26:16 +0100

    Fixes failing features test, asynchronicity problem of calling the tests

--- test/featureTest.js
+++ test/featureTest.js
@@ -7,7 +7,7 @@ function checkHeadOnPage() {
 };
 
 function checkNewsOnPage() {
-  var news = document.getElementsByClassName("news");
+  var news = document.getElementById("news").children;
   if (news.length === 0) {
     throw new Error ("There are no news!");
   } else {
@@ -16,10 +16,15 @@ function checkNewsOnPage() {
 };
 
 function checkHeadlinesOnPage() {
-  var headlines = document.getElementsByClassName("news")[0].children;
+  var headlines = document.getElementById("news").children;
   assert.isTrue(headlines[0].className === "headline", arguments.callee.name)
 };
 
-checkHeadOnPage();
-checkNewsOnPage();
-checkHeadlinesOnPage();
+(function(exports) {
+  function executeTests() {
+    checkHeadOnPage();
+    checkNewsOnPage();
+    checkHeadlinesOnPage();
+  }
+  exports.executeTests = executeTests;
+})(this);
