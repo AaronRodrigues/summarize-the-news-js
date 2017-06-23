bfd75739233f86c263c497bd00b3874a70118fd8
Michael Jacobson <michael.jacobson89@gmail.com>
2017-06-23 11:35:26 +0100

    • continue refactoring js

--- script.js
+++ script.js
@@ -31,47 +31,63 @@ function loadContent() {
         request.send();
     }
 
-    // Construct HTML elements, populate them with data from the API calls and render them on the view.
-    function renderArticle(articlesArray, articleIndex) {
+    // Grab article data from array of articles and assign to variables for later use.
+    function extractArticleData(articlesArray, articleIndex) {
+        headline = articlesArray[articleIndex].webTitle;
+        console.log(headline);
+        category = articlesArray[articleIndex].sectionName;
+        date = articlesArray[articleIndex].webPublicationDate;
+        url = articlesArray[articleIndex].webUrl;
+    }
 
-        // Grab article data from array of articles and assign to variables for later use.
-        var headline = articlesArray[articleIndex].webTitle;
-        var category = articlesArray[articleIndex].sectionName;
-        var date = articlesArray[articleIndex].webPublicationDate;
-        var url = articlesArray[articleIndex].webUrl;
+    // Create HTML elements to later populate with article data.
+    function createHtmlElements() {
+        article = document.createElement("article");
+        articleDetails = document.createElement("details");
+        textContent = document.createElement("p");
+        headlineHTML = document.createElement("summary");
+        categoryAndDateHTML = document.createElement("p");
+        lineBreak = document.createElement("br");
+    }
 
-        // Create HTML elements to later populate with article data.
-        var article = document.createElement("article");
-        var articleDetails = document.createElement("details");
-        var textContent = document.createElement("p");
-        var headlineHTML = document.createElement("summary");
-        var categoryAndDateHTML = document.createElement("p");
-        var lineBreak = document.createElement("br");
-
-        // Assign classes or IDs to newly generated HTML elements for testing and styling.
+    // Assign classes or IDs to newly generated HTML elements for testing and styling.
+    function designateClasses() {
         article.setAttribute("class", "article");
         headlineHTML.setAttribute("class", "headline");
         categoryAndDateHTML.setAttribute("class", "category-and-date");
         textContent.setAttribute("class", "text-content");
+    }
 
-        // Inject article data into HTMl elements.
+    // Structure separate HTML elements correctly inside article.
+    function constructArticleTree() {
+        news.appendChild(article);
+        console.log(news);
+        article.appendChild(categoryAndDateHTML);
+        article.appendChild(articleDetails);
+        articleDetails.appendChild(headlineHTML);
+        articleDetails.appendChild(textContent);
+        article.appendChild(lineBreak);
+    }
+
+    // Inject article data into HTMl elements.
+    function injectArticleData() {
         headlineHTML.innerHTML = headline;
         categoryAndDateHTML.innerHTML = category + " - " + new Date(date).toUTCString();
-
         summarizeText(url, function(summary) {
-            // Inject article data into HTMl elements (cont.)
             textContent.innerHTML = summary.join(" ");
-
-            // Structure separate HTML elements correctly inside article.
-            news.appendChild(article);
-            article.appendChild(categoryAndDateHTML);
-            article.appendChild(articleDetails);
-            articleDetails.appendChild(headlineHTML);
-            articleDetails.appendChild(textContent);
-            article.appendChild(lineBreak);
+            constructArticleTree();
         });
     }
 
-    retrieveData()
+    // Construct HTML elements, populate them with data from the API calls, organise them and render them on the view.
+    function renderArticle(articlesArray, articleIndex) {
+        var headline, category, date, url;
+        var article, articleDetails, textContent, headLineHTML, CategoryAndDateHTML, lineBreak;
+        extractArticleData(articlesArray, articleIndex);
+        createHtmlElements();
+        designateClasses();
+        injectArticleData();
+    }
 
+    retrieveData()
 }
